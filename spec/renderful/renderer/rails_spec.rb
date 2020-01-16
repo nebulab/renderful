0778f1b8e57f1a2a360a42307700d5cf5ce4ab13
Alessandro Desantis <desa.alessandro@gmail.com>
2020-01-16 09:40:33 +0100

    Test the Rails renderer in integration

--- spec/renderful/renderer/rails_spec.rb
+++ spec/renderful/renderer/rails_spec.rb
@@ -2,8 +2,6 @@
 
 require 'spec_helper'
 
-require 'action_controller'
-
 RSpec.describe Renderful::Renderer::Rails do
   subject(:renderer) do
     TestComponentRenderer.new(entry, client: client)
@@ -11,8 +9,6 @@ RSpec.describe Renderful::Renderer::Rails do
 
   let(:entry) { OpenStruct.new(content_type: OpenStruct.new(id: 'testComponent')) }
   let(:client) { instance_double('Renderful::Client') }
-
-  let(:rails_renderer) { instance_spy('ActionController::Renderer') }
 
   before(:all) do
     TestComponentRenderer = Class.new(described_class) do
@@ -22,45 +18,11 @@ RSpec.describe Renderful::Renderer::Rails do
     end
   end
 
-  before { allow(ActionController::Base).to receive(:renderer).and_return(rails_renderer) }
-
   describe '#render' do
-    it 'returns the output from the ActionController renderer' do
-      allow(rails_renderer).to receive(:render).and_return('render_output')
-
+    it 'renders the correct partial' do
       result = renderer.render
 
-      expect(result).to eq('render_output')
-    end
-
-    it 'infers the correct component view' do
-      renderer.render
-
-      expect(rails_renderer).to have_received(:render).with(a_hash_including(
-                                                              partial: 'renderful/test_component',
-                                                            ))
-    end
-
-    it 'passes locals to the view' do
-      renderer.render
-
-      expect(rails_renderer).to have_received(:render).with(a_hash_including(
-                                                              locals: a_hash_including(
-                                                                test_local: 'local_value',
-                                                              ),
-                                                            ))
-    end
-
-    it 'passes the entry, renderer and client to the view' do
-      renderer.render
-
-      expect(rails_renderer).to have_received(:render).with(a_hash_including(
-                                                              locals: a_hash_including(
-                                                                entry: entry,
-                                                                renderer: renderer,
-                                                                client: client,
-                                                              ),
-                                                            ))
+      expect(result.strip).to eq('test_local is local_value')
     end
   end
 end
