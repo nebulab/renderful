66571402858661982b4e41208dae7f9510956c46
Alessandro Desantis <desa.alessandro@gmail.com>
2020-04-01 17:16:28 +0200

    Remove the ability to resolve references from components

--- lib/renderful/component/base.rb
+++ lib/renderful/component/base.rb
@@ -5,14 +5,6 @@ module Renderful
     class Base
       attr_reader :entry, :client
 
-      class << self
-        def resolve(field)
-          define_method(field) do
-            resolve(entry.send(field))
-          end
-        end
-      end
-
       def initialize(entry, client:)
         @entry = entry
         @client = client
@@ -20,18 +12,6 @@ module Renderful
 
       def render
         raise NotImplementedError
-      end
-
-      private
-
-      def resolve(reference)
-        if reference.is_a?(Enumerable)
-          reference.map(&method(:resolve))
-        elsif reference.is_a?(Contentful::Link)
-          reference.resolve(client.contentful)
-        else
-          reference
-        end
       end
     end
   end
